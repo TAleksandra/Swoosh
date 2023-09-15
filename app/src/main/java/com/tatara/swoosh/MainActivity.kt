a512b70b30b991281726883163f2025bffec63ae
TAleksandra <tatara.ola@gmail.com>
2023-09-15 21:26:36 +0200

    lesson 4

--- app/src/main/java/com/tatara/swoosh/MainActivity.kt
+++ app/src/main/java/com/tatara/swoosh/MainActivity.kt
@@ -3,18 +3,15 @@ package com.tatara.swoosh
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import com.tatara.swoosh.databinding.ActivityMainBinding
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        binding.getStartedBtn.setOnClickListener {
+        val btn = findViewById<Button>(R.id.getStartedBtn)
+        btn.setOnClickListener {
             val leagueIntent = Intent(this, LeagueActivity::class.java)
             startActivity(leagueIntent)
         }
