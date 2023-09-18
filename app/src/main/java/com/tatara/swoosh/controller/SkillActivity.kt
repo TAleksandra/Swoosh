0e3cff3be3d5ca8c286e03a5a948c8996e40970e
TAleksandra <tatara.ola@gmail.com>
2023-09-18 23:17:29 +0200

    final

--- app/src/main/java/com/tatara/swoosh/controller/SkillActivity.kt
+++ app/src/main/java/com/tatara/swoosh/controller/SkillActivity.kt
@@ -12,6 +12,11 @@ import com.tatara.swoosh.utilities.EXTRA_PLAYER
 class SkillActivity : BaseActivity() {
 
     lateinit var player: Player
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
